60a3d8b0a3dbe192d7ba3de9608ded9c6fe64dc8
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-12-19 18:57:23 -0800

    Routing entre Paginas

--- PortfolioEzequielGiampaoli/src/app/components/logo-ap/logo-ap.component.ts
+++ PortfolioEzequielGiampaoli/src/app/components/logo-ap/logo-ap.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-ap',
   templateUrl: './logo-ap.component.html',
-  styleUrls: ['./logo-ap.component.css']
+  styleUrls: ['./logo-ap.component.css'],
 })
 export class LogoAPComponent {
+  constructor(private router: Router) {}
 
+  login() {
+    this.router.navigate(['login']);
+  }
 }
