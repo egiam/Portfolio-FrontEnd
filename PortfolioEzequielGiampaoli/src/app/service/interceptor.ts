67a8c1a942966d59f24af530e0f1e3e9f9aea226
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-12-20 21:12:07 -0800

    Realizando Login e ingreso de User

---
+++ PortfolioEzequielGiampaoli/src/app/service/interceptor.ts
@@ -0,0 +1,41 @@
+import { Observable } from 'rxjs';
+import { TokenService } from './token.service';
+import {
+  HTTP_INTERCEPTORS,
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Interceptor {
+  constructor(private tokenService: TokenService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    let intReq = req;
+    const token = this.tokenService.getToken();
+    if (token != null) {
+      intReq = req.clone({
+        headers: req.headers.set('Authorization', 'Bearer' + token),
+      });
+    }
+    return next.handle(intReq);
+  }
+}
+
+export const interceptorProvider = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: Interceptor,
+    multi: true,
+  },
+];
+function injectable(arg0: { providedIn: string }) {
+  throw new Error('Function not implemented.');
+}
